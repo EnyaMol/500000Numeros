3d2eb8a28e01fa0eed3b468d413da93850717d7e
Ayinee <onaienya@gmail.com>
2018-10-12 16:18:22 +0200

    Només parells i entre 700000

--- Elsnumeros.js
+++ Elsnumeros.js
@@ -1,14 +1,16 @@
 var numeros = [];
 var numero = prompt("Numero a buscar entre els aleatoris.");
 for (var i = 0 ; i< 500000; i++){
-    numeros[i]=Math.round(Math.random() * 500000);
+    numeros[i]=Math.round(Math.random() * 700000);
 }
 var trobat = false;
 var primertemps = new Date();
 for (var i=0; i<numeros.length; i++){
    if(numero == numeros[i]){
+       if(numero%2==0){
         trobat=true;
         var final = new Date();
+       }
    }
 }
 if(trobat==false){
