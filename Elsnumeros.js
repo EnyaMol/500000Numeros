6c83a882c31a605e5c69f7344a2817704f0aba97
Ayinee <onaienya@gmail.com>
2018-10-12 15:58:19 +0200

    Primer commit

---
+++ Elsnumeros.js
@@ -0,0 +1,20 @@
+var numeros = [];
+var numero = prompt("Numero a buscar entre els aleatoris.");
+for (var i = 0 ; i< 500000; i++){
+    numeros[i]=Math.round(Math.random() * 500000);
+}
+var trobat = false;
+var primertemps = new Date();
+for (var i=0; i<numeros.length; i++){
+   if(numero == numeros[i]){
+        trobat=true;
+        var final = new Date();
+   }
+}
+if(trobat==false){
+    alert("No trobat");
+}
+var temps = (final-primertemps)/3600;
+if (trobat==true){
+    alert("L'ha trobat i ha trigat " + temps);
+}
